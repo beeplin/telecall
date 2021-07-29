685ed05f3b8be073998c6a19464b67fceffc8d15
beeplin <beeplin@gmail.com>
2021-07-29 21:56:47 +0800

    chore: no lib

--- lib/server.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Fn, TeleRequest, TeleResponse } from './types'
-export declare function handleTeleRequest<T extends Fn>(
-  request: TeleRequest<T>,
-  ctx: unknown,
-): Promise<{
-  status: number
-  json: TeleResponse<T>
-}>
-//# sourceMappingURL=server.d.ts.map
