101e65713cac42c97247670612c055939b3eccfd
beeplin <beeplin@gmail.com>
2021-08-03 00:57:06 +0800

    feat: ast

--- plugins/__tests__/convert-import.test.js
+++ plugins/__tests__/convert-import.test.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-eval */
 
-const { parseImportString, convertImport } = require('../convert-import')
+const {
+  parseImportString,
+  convertImportByRegex,
+  convertImportByAst,
+} = require('../convert-import')
 
 describe('parseImportString', () => {
   it('a', () => {
@@ -101,9 +105,9 @@ describe('parseImportString', () => {
   })
 })
 
-describe('convertImport', () => {
+describe('convertImportByRegex', () => {
   it('single import', () => {
-    const converted = convertImport(`
+    const converted = convertImportByRegex(`
     import{ a as b,c,d as eee},def,* as aaa from"path"
     module.exports = {b, c, eee, def, aaa}
     `)
@@ -122,7 +126,7 @@ describe('convertImport', () => {
   })
 
   it('multiple imports', () => {
-    const converted = convertImport(`
+    const converted = convertImportByRegex(`
     const bbb = 1
     import { a as b,c,d as eee }, def, * as aaa   from "path"
     import a2, {b2, c as c2}, * as d2  from "path2"
@@ -148,7 +152,7 @@ describe('convertImport', () => {
   })
 
   it('multiple imports with semicolons and line breaks', () => {
-    const converted = convertImport(`
+    const converted = convertImportByRegex(`
     const bbb = 1
     import { a as b,c,d as eee }, def, * as aaa   from "path"; import a2, {b2, c as c2}, * as d2  from "path2"
     const ccc = 1
@@ -160,11 +164,6 @@ describe('convertImport', () => {
     * as d4  from "path4"
     module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2, b3, c3, eee3, def3, aaa3, a4, b4, c4, d4}
     `)
-
-    console.log(
-      '🚀 ~ file: convert-import.test.js ~ line 199 ~ it ~ converted',
-      converted,
-    )
     eval(converted)
 
     expect(module.exports).toMatchObject({
@@ -197,13 +196,216 @@ describe('convertImport', () => {
     expect(module.exports.d4.abcd).toStrictEqual({ path: 'path4', name: 'abcd' })
   })
 
-  it.skip('multiple imports with semicolons and imports in strings', () => {
-    const converted = convertImport(`
+  it('multiple imports with semicolons and comments', () => {
+    const converted = convertImportByRegex(`
     const bbb = 1
-    const fake = 'import { a as b,c,d as eee }, def, * as aaa   from "path"; import a2, {b2, c as c2}, * as d2  from "path2"'
     import { a as b,c,d as eee }, def, * as aaa   from "path"; import a2, {b2, c as c2}, * as d2  from "path2"
+    // import { a as b,c,d as eee }, def, * as aaa   from "path"; import a2, {b2, c as c2}, * as d2  from "path2"
     const ccc = 1
-    import { a as b3,c3,d as eee3 }, def3, * as aaa3   from "path3"; const ddd = 1; import a4, {b4, c as c4}, * as d4  from "path4"
+    import { 
+      a as b3,
+      // test
+      c3,
+      /* aaa */
+      d as eee3 
+    }, def3, * as aaa3   from "path3"; const ddd = 1; import a4, {b4, c as c4}, 
+    * as d4  from "path4"
+    module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2, b3, c3, eee3, def3, aaa3, a4, b4, c4, d4}
+    `)
+    eval(converted)
+
+    expect(module.exports).toMatchObject({
+      b: { path: 'path', name: 'a' },
+      c: { path: 'path', name: 'c' },
+      eee: { path: 'path', name: 'd' },
+      def: { path: 'path', name: 'default' },
+      a2: { path: 'path2', name: 'default' },
+      b2: { path: 'path2', name: 'b2' },
+      c2: { path: 'path2', name: 'c' },
+      b3: { path: 'path3', name: 'a' },
+      c3: { path: 'path3', name: 'c3' },
+      eee3: { path: 'path3', name: 'd' },
+      def3: { path: 'path3', name: 'default' },
+      a4: { path: 'path4', name: 'default' },
+      b4: { path: 'path4', name: 'b4' },
+      c4: { path: 'path4', name: 'c' },
+    })
+    expect(module.exports.aaa.xxx).toStrictEqual({ path: 'path', name: 'xxx' })
+    expect(module.exports.aaa.yy).toStrictEqual({ path: 'path', name: 'yy' })
+    expect(module.exports.aaa.a).toStrictEqual({ path: 'path', name: 'a' })
+    expect(module.exports.aaa.d).toStrictEqual({ path: 'path', name: 'd' })
+    expect(module.exports.d2.d).toStrictEqual({ path: 'path2', name: 'd' })
+    expect(module.exports.d2.abcd).toStrictEqual({ path: 'path2', name: 'abcd' })
+    expect(module.exports.aaa3.xxx).toStrictEqual({ path: 'path3', name: 'xxx' })
+    expect(module.exports.aaa3.yy).toStrictEqual({ path: 'path3', name: 'yy' })
+    expect(module.exports.aaa3.a).toStrictEqual({ path: 'path3', name: 'a' })
+    expect(module.exports.aaa3.d).toStrictEqual({ path: 'path3', name: 'd' })
+    expect(module.exports.d4.d).toStrictEqual({ path: 'path4', name: 'd' })
+    expect(module.exports.d4.abcd).toStrictEqual({ path: 'path4', name: 'abcd' })
+  })
+})
+
+describe('convertImportByAst', () => {
+  it('single import', () => {
+    const converted = convertImportByAst(`
+    import def,{a as b,c,d as eee} from "path"
+    import * as aaa from 'path'
+    module.exports = {b, c, eee, def, aaa}
+    `)
+    eval(converted)
+
+    expect(module.exports).toMatchObject({
+      b: { path: 'path', name: 'a' },
+      c: { path: 'path', name: 'c' },
+      eee: { path: 'path', name: 'd' },
+      def: { path: 'path', name: 'default' },
+    })
+    expect(module.exports.aaa.xxx).toStrictEqual({ path: 'path', name: 'xxx' })
+    expect(module.exports.aaa.yy).toStrictEqual({ path: 'path', name: 'yy' })
+    expect(module.exports.aaa.a).toStrictEqual({ path: 'path', name: 'a' })
+    expect(module.exports.aaa.d).toStrictEqual({ path: 'path', name: 'd' })
+  })
+
+  it('multiple imports', () => {
+    const converted = convertImportByAst(`
+    const bbb = 1
+    import def,{a as b,c,d as eee} from "path"
+    import * as aaa from 'path'
+    import a2, {b2, c as c2}   from "path2"
+    import  * as d2  from "path2"
+    module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2}
+    `)
+    eval(converted)
+
+    expect(module.exports).toMatchObject({
+      b: { path: 'path', name: 'a' },
+      c: { path: 'path', name: 'c' },
+      eee: { path: 'path', name: 'd' },
+      def: { path: 'path', name: 'default' },
+      a2: { path: 'path2', name: 'default' },
+      b2: { path: 'path2', name: 'b2' },
+      c2: { path: 'path2', name: 'c' },
+    })
+    expect(module.exports.aaa.xxx).toStrictEqual({ path: 'path', name: 'xxx' })
+    expect(module.exports.aaa.yy).toStrictEqual({ path: 'path', name: 'yy' })
+    expect(module.exports.aaa.a).toStrictEqual({ path: 'path', name: 'a' })
+    expect(module.exports.aaa.d).toStrictEqual({ path: 'path', name: 'd' })
+    expect(module.exports.d2.d).toStrictEqual({ path: 'path2', name: 'd' })
+    expect(module.exports.d2.abcd).toStrictEqual({ path: 'path2', name: 'abcd' })
+  })
+
+  it('multiple imports with semicolons and line breaks', () => {
+    const converted = convertImportByAst(`
+    const bbb = 1
+    import def, { a as b,c,d as eee }  from "path"; import a2, {b2, c as c2}  from "path2"
+    import * as aaa   from "path"; import * as d2  from "path2"
+    const ccc = 1
+    import def3, { 
+      a as b3,
+      c3,
+      d as eee3 
+    }  from "path3"; import * as aaa3   from "path3"; const ddd = 1; import a4, {b4, c as c4} 
+     from "path4"
+    import * as d4  from "path4"
+    module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2, b3, c3, eee3, def3, aaa3, a4, b4, c4, d4}
+    `)
+    eval(converted)
+
+    expect(module.exports).toMatchObject({
+      b: { path: 'path', name: 'a' },
+      c: { path: 'path', name: 'c' },
+      eee: { path: 'path', name: 'd' },
+      def: { path: 'path', name: 'default' },
+      a2: { path: 'path2', name: 'default' },
+      b2: { path: 'path2', name: 'b2' },
+      c2: { path: 'path2', name: 'c' },
+      b3: { path: 'path3', name: 'a' },
+      c3: { path: 'path3', name: 'c3' },
+      eee3: { path: 'path3', name: 'd' },
+      def3: { path: 'path3', name: 'default' },
+      a4: { path: 'path4', name: 'default' },
+      b4: { path: 'path4', name: 'b4' },
+      c4: { path: 'path4', name: 'c' },
+    })
+    expect(module.exports.aaa.xxx).toStrictEqual({ path: 'path', name: 'xxx' })
+    expect(module.exports.aaa.yy).toStrictEqual({ path: 'path', name: 'yy' })
+    expect(module.exports.aaa.a).toStrictEqual({ path: 'path', name: 'a' })
+    expect(module.exports.aaa.d).toStrictEqual({ path: 'path', name: 'd' })
+    expect(module.exports.d2.d).toStrictEqual({ path: 'path2', name: 'd' })
+    expect(module.exports.d2.abcd).toStrictEqual({ path: 'path2', name: 'abcd' })
+    expect(module.exports.aaa3.xxx).toStrictEqual({ path: 'path3', name: 'xxx' })
+    expect(module.exports.aaa3.yy).toStrictEqual({ path: 'path3', name: 'yy' })
+    expect(module.exports.aaa3.a).toStrictEqual({ path: 'path3', name: 'a' })
+    expect(module.exports.aaa3.d).toStrictEqual({ path: 'path3', name: 'd' })
+    expect(module.exports.d4.d).toStrictEqual({ path: 'path4', name: 'd' })
+    expect(module.exports.d4.abcd).toStrictEqual({ path: 'path4', name: 'abcd' })
+  })
+
+  it('multiple imports with semicolons and comments', () => {
+    const converted = convertImportByAst(`
+    const bbb = 1
+    // comment
+    import def, { a as b,c,d as eee }  from "path"; import a2, {b2, c as c2}  from "path2"
+    import * as aaa /* comment */  from "path"; import * as d2  from "path2"
+    const ccc = 1
+    import def3, { 
+      a as b3,
+      c3, /* comment */
+      d as eee3 
+      // comment
+    }  from "path3"; import * as aaa3   from "path3"; const ddd = 1; import a4, {b4, c as c4} 
+     from "path4"
+    import * as d4  from "path4"
+    module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2, b3, c3, eee3, def3, aaa3, a4, b4, c4, d4}
+    `)
+    eval(converted)
+
+    expect(module.exports).toMatchObject({
+      b: { path: 'path', name: 'a' },
+      c: { path: 'path', name: 'c' },
+      eee: { path: 'path', name: 'd' },
+      def: { path: 'path', name: 'default' },
+      a2: { path: 'path2', name: 'default' },
+      b2: { path: 'path2', name: 'b2' },
+      c2: { path: 'path2', name: 'c' },
+      b3: { path: 'path3', name: 'a' },
+      c3: { path: 'path3', name: 'c3' },
+      eee3: { path: 'path3', name: 'd' },
+      def3: { path: 'path3', name: 'default' },
+      a4: { path: 'path4', name: 'default' },
+      b4: { path: 'path4', name: 'b4' },
+      c4: { path: 'path4', name: 'c' },
+    })
+    expect(module.exports.aaa.xxx).toStrictEqual({ path: 'path', name: 'xxx' })
+    expect(module.exports.aaa.yy).toStrictEqual({ path: 'path', name: 'yy' })
+    expect(module.exports.aaa.a).toStrictEqual({ path: 'path', name: 'a' })
+    expect(module.exports.aaa.d).toStrictEqual({ path: 'path', name: 'd' })
+    expect(module.exports.d2.d).toStrictEqual({ path: 'path2', name: 'd' })
+    expect(module.exports.d2.abcd).toStrictEqual({ path: 'path2', name: 'abcd' })
+    expect(module.exports.aaa3.xxx).toStrictEqual({ path: 'path3', name: 'xxx' })
+    expect(module.exports.aaa3.yy).toStrictEqual({ path: 'path3', name: 'yy' })
+    expect(module.exports.aaa3.a).toStrictEqual({ path: 'path3', name: 'a' })
+    expect(module.exports.aaa3.d).toStrictEqual({ path: 'path3', name: 'd' })
+    expect(module.exports.d4.d).toStrictEqual({ path: 'path4', name: 'd' })
+    expect(module.exports.d4.abcd).toStrictEqual({ path: 'path4', name: 'abcd' })
+  })
+
+  it('multiple imports with semicolons and imports in strings', () => {
+    const converted = convertImportByAst(`
+    const bbb = 1
+    // comment
+    const string = \`import def, { a as b,c,d as eee }  from "path"; import a2, {b2, c as c2}  from "path2"\`
+    import def, { a as b,c,d as eee }  from "path"; import a2, {b2, c as c2}  from "path2"
+    import * as aaa /* comment */  from "path"; import * as d2  from "path2"
+    const ccc = 1
+    import def3, { 
+      a as b3,
+      c3, /* comment */
+      d as eee3 
+      // comment
+    }  from "path3"; import * as aaa3   from "path3"; const ddd = 1; import a4, {b4, c as c4} 
+     from "path4"
+    import * as d4  from "path4"
     module.exports = {b, c, eee, def, aaa, a2, b2, c2, d2, b3, c3, eee3, def3, aaa3, a4, b4, c4, d4}
     `)
     console.log(
--- plugins/convert-import.js
+++ plugins/convert-import.js
@@ -1,5 +1,9 @@
+const acorn = require('acorn')
+
 function parseImportString(string) {
   const stringMatch = string
+    .replace(/\/\/.*?\n/gu, '\n')
+    .replace(/\/\*.*?\*\//gu, '')
     .replace(/\n/gu, '')
     .trim()
     .match(/^(.*?)\{(.*?)\}(.*?)$/u)
@@ -23,21 +27,59 @@ function parseImportString(string) {
   return [allName, defaultName, ...namedName].filter((i) => i?.local)
 }
 
-function convertImport(code) {
+function convertImportByRegex(code) {
   // NOTE does not work for `import` in strings
   return code.replace(
     /(?<pre>\s+)import(?<string>[\s{].*?[\s}])from\s*(?<quote>['"`])(?<path>.*?)\k<quote>(?<post>\s*[\n;])/gsu,
     // eslint-disable-next-line max-params
     (_, pre, string, __, path, post) => {
       const names = parseImportString(string)
-      const replaced = names.reduce((acc, { local, imported }) => {
-        return imported === '*'
-          ? `${acc}const ${local} = new Proxy({}, { get: function(t, p) { return { path: '${path}', name: p }}}); `
-          : `${acc}const ${local} = { path: '${path}', name: '${imported}' }; `
-      }, '')
+      const replaced = buildReplacedString(path, names)
       return pre + replaced + post
     },
   )
 }
 
-module.exports = { parseImportString, convertImport }
+function parstImportAst(node) {
+  if (node.type !== 'ImportDeclaration')
+    throw new Error('node should be import declaration')
+  return {
+    path: node.source.value,
+    names: node.specifiers.map(({ type, imported, local }) => ({
+      local: local.name,
+      imported:
+        type === 'ImportNamespaceSpecifier'
+          ? '*'
+          : type === 'ImportDefaultSpecifier'
+          ? 'default'
+          : imported.name,
+    })),
+  }
+}
+
+function buildReplacedString(path, names) {
+  return names.reduce((acc, { local, imported }) => {
+    return imported === '*'
+      ? `${acc}const ${local} = new Proxy({}, { get: function(t, p) { return { path: '${path}', name: p }}}); `
+      : `${acc}const ${local} = { path: '${path}', name: '${imported}' }; `
+  }, '')
+}
+
+function convertImportByAst(code) {
+  return acorn
+    .parse(code, { sourceType: 'module' })
+    .body.filter((node) => node.type === 'ImportDeclaration')
+    .reverse()
+    .reduce((acc, node) => {
+      const { path, names } = parstImportAst(node)
+      const replaced = buildReplacedString(path, names)
+      return acc.slice(0, node.start) + replaced + acc.slice(node.end)
+    }, code)
+}
+
+module.exports = {
+  parseImportString,
+  convertImportByRegex,
+  parstImportAst,
+  convertImportByAst,
+}
